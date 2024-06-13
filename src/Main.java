aeb837df55ee6d4d67aa9a7bf0584c464b584abe
Jaywint7 <151880536+Jaywint7@users.noreply.github.com>
2024-06-12 21:01:51 -0500

    upd

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        ejemplo per = new ejemplo("Luis");
+        per.hola();
+    }
+}
--- src/ejemplo.java
+++ src/ejemplo.java
@@ -1,2 +1,11 @@
 public class ejemplo {
+    public String Nombre;
+
+    public ejemplo(String nombre){
+        this.Nombre=nombre;
+    }
+
+    public void hola(){
+        System.out.println("Hola, su nombre es: "+Nombre);
+    }
 }
